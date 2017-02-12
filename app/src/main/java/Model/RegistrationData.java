f7dbb52b168c3db257623289631a69853504e754
anderson-ryan <anderson.ryan473@gmail.com>
2017-02-12 16:51:57 -0500

    Added User object. Changed RegistrationData to work with new User object

--- app/src/main/java/Model/RegistrationData.java
+++ app/src/main/java/Model/RegistrationData.java
@@ -1,41 +1,50 @@
 package Model;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.security.SecureRandom;
 /**
  * Created by ander_000 on 2/12/2017.
  */
 
 public class RegistrationData {
-    private static HashMap<String, String> RegData = new HashMap<>();
+    private static ArrayList<User> RegData;
 
     public RegistrationData() {
-        RegData.put("user", "pass");
+        RegData.add(new User("user", "pass"));
     }
 
     public boolean addUser(String user, String password) {
-        if (!RegData.containsKey(user)) {
-            RegData.put(user, password);
+        User temp = new User(user, password);
+        if (!RegData.contains(temp)) {
+            RegData.add(temp);
             return true;
         }
         return false;
     }
 
     public boolean removeUser(String user) {
-        if (RegData.containsKey(user)) {
-            RegData.remove(user);
-            return true;
+        User temp = new User(user, "");
+        for (User name : RegData) {
+            if (name.getName().equals(user)) {
+                RegData.remove(name);
+                return true;
+            }
         }
         return false;
     }
 
     public boolean userExists(String user) {
-        if (RegData.containsKey(user)) {
-            return true;
+        User temp = new User(user, "");
+        for (User name : RegData) {
+            if (name.getName().equals(user)) {
+                return true;
+            }
         }
         return false;
     }
 
     public boolean checkPassword(String user, String pass) {
-        if (RegData.get(user).equals(pass)) {
+        User temp = new User(user, pass);
+        if (RegData.contains(temp)) {
             return true;
         }
         return false;
