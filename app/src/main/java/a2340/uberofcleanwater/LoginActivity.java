c082efc4b33531e853b06965be0865181969d99f
rscsam <sam.costley@yahoo.com>
2017-02-12 16:46:16 -0500

    Created WelcomeActivity and finished activity_login.xml

--- app/src/main/java/a2340/uberofcleanwater/LoginActivity.java
+++ app/src/main/java/a2340/uberofcleanwater/LoginActivity.java
@@ -1,7 +1,9 @@
 package a2340.uberofcleanwater;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void registerOnClick(View view) {
+
+    }
+
+    public void loginOnClick(View view) {
+        
+    }
+
+    private void startWelcomeActivity() {
+        Intent welcomeActivity = new Intent(this, WelcomeActivity.class);
+        startActivity(welcomeActivity);
     }
 }
